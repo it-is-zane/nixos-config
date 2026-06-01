536614e21e3a306323af6731a8e0004cf108c646
Zane Gant <zane.gant@gmail.com>
2026-06-01 15:35:32 -0700

    adding gamescope
    satisfactory is acting weird(rendering to steam's window) thought i might try to see if using gamescope would help

--- users/zane.nix
+++ users/zane.nix
@@ -62,8 +62,13 @@
     '';
   };
 
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
