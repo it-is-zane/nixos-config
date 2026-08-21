594712439bfd48e26ef89949fd8ba4b05a5823f1
Zane Gant <zane.gant@gmail.com>
2026-08-20 18:20:12 -0700

    removed x11 and did some organization

--- configuration.nix
+++ configuration.nix
@@ -9,24 +9,22 @@
   ...
 }:
 {
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    loader = {
+      # Bootloader.
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 16;
+      };
+      efi.canTouchEfiVariables = true;
+    };
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
+    # Use latest kernel.
+    kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+    initrd.kernelModules = [ "amdgpu" ];
+  };
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -46,23 +44,40 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-  # services.xserver.videoDrivers = [ "amdgpu" ];
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    wireless.enable = true; # Enables wireless support via wpa_supplicant.
+    interfaces.wlp15s0.ipv6.addresses = [
+      {
+        # ======Subnet=======
+        # 2606:c800:6095:4300::5fe
+        address = "2606:c800:6095:4300::5fe";
+        prefixLength = 64;
+      }
+    ];
 
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
+    # Enable networking
+    networkmanager.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    firewall.enable = false;
   };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  hardware.bluetooth.enable = true;
+  home-manager.sharedModules = [
+    {
+      services.mpris-proxy.enable = true;
+    }
+  ];
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -83,8 +98,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  hardware.bluetooth.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -103,15 +116,6 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -119,5 +123,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
